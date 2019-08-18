62e5aafecd9c9032a373d876504de10171f8c7aa
sachinlamba <lamba92sachin@gmail.com>
2019-08-18 15:09:21 +0530

    CRA project with Redux structure.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import { simpleAction } from './modules/actions/simpleAction';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+
+  render() {
+    return (
+      <div className = "App" >
+        <header className = "App-header" >
+          <h1 className = "App-title" > Welcome to React < /h1>
+        </header>
+        <p className = "App-intro" >
+          To get started, edit < code > src / App.js < /code> and save to reload
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <pre > { JSON.stringify(this.props) } </pre>
+        <button onClick = {this.simpleAction} > Test redux action </button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  Provider
+} from 'react-redux'
+import configureStore from './modules/store/store';
 import './index.css';
 import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render( <
+  Provider store = {
+    configureStore()
+  } >
+    <App / >
+  </Provider>,
+  document.getElementById('root')
+);
+// registerServiceWorker();
 serviceWorker.unregister();
+
+//https://medium.com/backticks-tildes/setting-up-a-redux-project-with-create-react-app-e363ab2329b8
